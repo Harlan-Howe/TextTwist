b884b2c8f130e29480cd39372e2a43290c3cd727
Harlan Howe <67292973+Harlan-Howe@users.noreply.github.com>
2023-02-10 14:58:19 -0600

    Initial commit

---
+++ src/TextTwister.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class TextTwister
+{
+
+    public ArrayList<String> twistWord(String wordToTwist)
+    {
+        ArrayList<String> result = null;
+        // TODO: write this method.
+        // This is almost certainly a wrapper method that should
+        // call a recursive method that returns an ArrayList of
+        // Strings that you will subsequently return.
+
+        return result;
+    }
+
+    // TODO: write a recursive method that receives a String for
+    //  the "prefix" and the String for the "pool" and returns
+    //  an ArrayList of all the possible "words" that start with the
+    //  prefix and contain some, none, or all of the letters in the pool.
+
+    // TODO: (optional) given an ArrayList of "words," returns another
+    //  ArrayList of all the words on this list that are actually found
+    //  in the dictionary.
+
+}
